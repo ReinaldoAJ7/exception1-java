d2c5d4890693810676e6890c14977b5534ca1c92
Reinaldo Almeida de Jesus <reinaldojesus238@gmail.com>
2023-08-31 10:16:49 -0300

    Very bad solution

---
+++ src/excepionsAula173/entities/Reservation.java
@@ -0,0 +1,58 @@
+package excepionsAula173.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public Long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut){
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" +
+                "\n***************** - New reservation - *****************" +
+                "\nRoom number: " + roomNumber +
+                "\nCheck in: " + checkIn +
+                "\nCheck out: " + checkOut+
+                "\nDuration "+duration()+" nights" +
+                "\n=========================================================\n";
+    }
+}
